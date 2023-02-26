e47cfade03790528d64bf31f366c9e7d81cc0790
Miggi <miggi@noreply.familyhainz.de>
2023-02-26 11:49:36 +0100

    :lipstick: VersionChecker SnackBar

--- lib/util/version_checker.dart
+++ lib/util/version_checker.dart
@@ -12,31 +12,42 @@ class VersionChecker {
     debugPrint("Version is $version");
 
     requestReleases(3)
-        .then((releases) => compareVersion(context, releases, version))
+        .then((releases) => _compareVersion(context, releases, version))
         .onError((error, stackTrace) => debugPrint(error.toString()));
   }
 
-  void compareVersion(BuildContext context, List<Release> releases, String version) {
+  void _compareVersion(BuildContext context, List<Release> releases, String version) {
     bool isNewest = (releases[0].tag_name.compareTo(version) == 0);
     String url =
         releases[0].assets.isNotEmpty ? releases[0].assets[0].browser_download_url : releases[0].html_url;
-    if (isNewest) {
+    if (!isNewest) {
       String msg = "Neue Version ${releases[0].tag_name} verfügbar";
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-          content: Row(
-        children: [Text(msg), TextButton(onPressed: () => _launchUrl(context, url), child: Text("Öffnen"))],
-      )));
+      Duration duration = const Duration(seconds: 5);
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(duration: duration, content: _buildRow(msg, context, url)));
     }
+  }
+
+  Row _buildRow(String msg, BuildContext context, String url) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(msg),
+        TextButton(
+            onPressed: () => _launchUrl(context, url),
+            child: const Text("Öffnen", style: TextStyle(color: Colors.blueAccent)))
+      ],
+    );
   }
 
   Future<void> _launchUrl(BuildContext context, String movieURL) async {
     final Uri url = Uri.parse(movieURL);
     if (!await launchUrl(url, mode: LaunchMode.externalApplication)) {
-      showError(context, url);
+      _showError(context, url);
     }
   }
 
-  bool showError(BuildContext context, Uri url) {
+  bool _showError(BuildContext context, Uri url) {
     ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Could not launch $url')));
     return true;
   }
